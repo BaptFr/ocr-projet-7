e05f3939dca7d104fd24eff428f99b3e0ecd4b14
BaptFr <baptistesalazar@gmail.com>
2024-04-09 11:25:35 +0200

    server simple

--- backend/app.js
+++ backend/app.js
@@ -2,23 +2,28 @@ const express = require ('express');
 
 const app = express();
 
-app.use((req, res, next ) => {
-    console.log('Requete reçue');
-    next();
-});
-
-app.use((raq, res, next) => {
-    res.status(201);
-    next();
-});
-
-app.use((req, res, next ) => {
-    res.json({message: 'TEST requete POSTMAN'});
-    next();
-});
+app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log('succès de la réponse');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.post('/api/auth/signup', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({ 
+        message : 'TEST INSCRIPT requête POST OK'
+    });
 });
 
-module.exports = app;
+app.get('/api/books', (req, res, next) => {
+    console.log(req.body);
+    res.status(200).json({ 
+        message : 'TEST GET requête  POST OK'
+    });
+});
+
+
+  module.exports = app;
