4948e954acf034166da3ffbc07a0ff4d93b321b2
BaptFr <baptistesalazar@gmail.com>
2024-04-15 14:22:00 +0200

    routes router + controller books créé

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,32 @@
 const express = require ('express');
+const bodyParser = require ('body-parser');
+const mongoose = require('mongoose');
+const Book = require('./models/Book')
 
+
+const bookRoutes = require('./routes/book');
+
+mongoose.connect('mongodb+srv://baptistesalazar:OcrMongo@cluster0.vuo5qlp.mongodb.net/',
+
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
+  
 const app = express();
 
 app.use(express.json());
-
+ 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
-
-app.post('/api/auth/signup', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({ 
-        message : 'TEST INSCRIPT requête POST OK'
-    });
 });
 
-app.get('/api/books', (req, res, next) => {
-    console.log(req.body);
-    res.status(200).json({ 
-        message : 'TEST GET requête  POST OK'
-    });
-});
+app.use(bodyParser.json());
+
+app.use('/api/books', bookRoutes);
 
 
-  module.exports = app;
+module.exports = app;
